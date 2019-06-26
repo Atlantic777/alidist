03c7bae4a2c7691e413c29de6f24d0308143177b
Nikola Hardi <nhardi@cern.ch>
2019-06-26 14:44:48 +0200

    Add defaults-next-alien

--- defaults-jalien.sh
+++ defaults-jalien.sh
@@ -1,54 +1,24 @@
-package: defaults-jalien
+package: defaults-next-alien
 version: v1
 disable:
   - arrow
-  - treelite
 env:
   CXXFLAGS: "-fPIC -g -O2 -std=c++11"
   CFLAGS: "-fPIC -g -O2"
   CMAKE_BUILD_TYPE: "RELWITHDEBINFO"
 overrides:
-  XRootD:
-    version: "%(tag_basename)s_JALIEN"
-    tag: "v4.8.6"
-    source: https://github.com/xrootd/xrootd
+  AliEn-ROOT-Legacy:
+    tag: "0.0.8-beta"
     build_requires:
-      - CMake
-    requires:
-      - "GCC-Toolchain:(?!osx)"
-      - "OpenSSL:(?!osx)"
-      - "osx-system-openssl:(osx.*)"
-      - ApMon-CPP
-      - libxml2
-  JDK:
-    version: "12.0.1_JALIEN"
-  libxml2:
-    version: "%(tag_basename)s_JALIEN"
-    overrides:
-    requires:
-      - zlib
-    build_requires:
-      - autotools
-      - "GCC-Toolchain:(?!osx)"
-  OpenSSL:
-    version: "v1.0.2o_JALIEN"
-    overrides:
-    requires:
-      - zlib
-    build_requires:
-      - "GCC-Toolchain:(?!osx)"
-  ApMon-CPP:
-    version: "%(tag_basename)s"
-    requires:
-      - "GCC-Toolchain:(?!osx)"
-    build_requires:
-      - autotools
+      - xalienfs
+      - Alice-GRID-Utils
   AliPhysics:
     version: "%(tag_basename)s_JALIEN"
     tag: v5-09-50-01
   AliRoot:
     version: "%(tag_basename)s_JALIEN"
-    tag: v5-09-50
+    source: https://github.com/atlantic777/aliroot
+    tag: v5-09-49_JALIEN
     requires:
       - ROOT
       - DPMJET
@@ -56,7 +26,7 @@ overrides:
       - GEANT3
       - GEANT4_VMC
       - Vc
-      - JAliEn-ROOT
+      - AliEn-ROOT-Legacy
 ---
 # This file is included in any build recipe and it's only used to set
 # environment variables. Which file to actually include can be defined by the
