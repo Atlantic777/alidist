f4db763fa960945f22749fd0e9c55db16cc85b48
Nikola Hardi <nikola.hardi@cern.ch>
2019-07-02 16:54:25 +0200

    Bump patched AliRoot tag for next-alien and jalien

--- defaults-next-alien.sh
+++ defaults-next-alien.sh
@@ -18,7 +18,7 @@ overrides:
   AliRoot:
     version: "%(tag_basename)s_NEXT_ALIEN"
     source: https://github.com/atlantic777/aliroot
-    tag: v5-09-49_JALIEN-01
+    tag: v5-09-49_JALIEN-02
     requires:
       - ROOT
       - DPMJET
